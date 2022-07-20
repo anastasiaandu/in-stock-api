3a1ad9efe6234a57fa54e738231b7a148c77d318
Saloni Jindal <salonijindal@MacBook-Pro.ht.home>
2022-07-20 17:55:13 -0400

    Patch method, first commit

--- routes/warehouses.js
+++ routes/warehouses.js
@@ -1,24 +1,26 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const fs = require('fs');
-const uniqid = require('uniqid');
-
+const fs = require("fs");
+const uniqid = require("uniqid");
 
 //create function to read warehouses file
 const readWarehouses = () => {
-    const warehousesDataFile = fs.readFileSync('./data/warehouses.json');
-    const warehousesData = JSON.parse(warehousesDataFile);
-    return warehousesData;
-}
-
+  const warehousesDataFile = fs.readFileSync("./data/warehouses.json");
+  const warehousesData = JSON.parse(warehousesDataFile);
+  return warehousesData;
+};
 
 //create endpoint to get all warehouses
 //GET
-router
-    .get('/', (req, res) => {
-        const warehousesData = readWarehouses();
-        
-        res.status(200).json(warehousesData);
-    });
+router.get("/", (req, res) => {
+  const warehousesData = readWarehouses();
 
+  res.status(200).json(warehousesData);
+});
+
+//Patch
+app.patch("/warehouse/:id", (req, res) => {
+  const warehouseID = req.params.id;
+  //   res.json(students);
+});
 module.exports = router;
