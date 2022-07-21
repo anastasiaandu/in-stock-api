3d49b0bd1c1a1a6538ba12b5fcbed93bd1a92d28
Saloni Jindal <salonijindal@MacBook-Pro.ht.home>
2022-07-21 08:17:39 -0400

    PATCH is working

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const inventoriesRoutes = require('./routes/inventories');
-const warehousesRoutes = require('./routes/warehouses');
-const cors = require('cors');
+const inventoriesRoutes = require("./routes/inventories");
+const warehousesRoutes = require("./routes/warehouses");
+const cors = require("cors");
 
-require('dotenv').config();
-const PORT = process.env.PORT || 8080;
+require("dotenv").config();
+const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
 app.use(cors());
 
 // app.use('/inventories', inventoriesRoutes);
-app.use('/warehouses', warehousesRoutes);
+app.use("/warehouses", warehousesRoutes);
 
-app
-    .listen(PORT, () => {
-        console.log(`Server is listening on ${PORT}`);
-    })
+app.listen(PORT, () => {
+  console.log(`Server is listening on ${PORT}`);
+});
--- routes/warehouses.js
+++ routes/warehouses.js
@@ -45,8 +45,28 @@ router.get("/:id", (req, res) => {
 });
 
 //Patch
-router.patch("/warehouse/:id", (req, res) => {
+router.patch("/:id", (req, res) => {
+  const warehousesData = readWarehouses();
   const warehouseID = req.params.id;
-  //   res.json(students);
+  const warehouse = warehousesData.find(
+    (warehouse) => warehouse.id === warehouseID
+  );
+
+  if (!warehouse)
+    return res.status(404).json({ message: "Warehouse Not Found" });
+
+  console.log("Requested body", req.body);
+  warehouse.name = req.body.name;
+  warehouse.address = req.body.address;
+  warehouse.city = req.body.city;
+  warehouse.country = req.body.country;
+  warehouse.contact.name = req.body.contact.name;
+  warehouse.contact.position = req.body.contact.position;
+  warehouse.contact.phone = req.body.contact.phone;
+  warehouse.contact.email = req.body.contact.email;
+
+  fs.writeFileSync("./data/warehouses.json", JSON.stringify(warehousesData));
+
+  res.status(202).json(warehouse);
 });
 module.exports = router;
